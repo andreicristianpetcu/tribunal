81f796025f03c80ff9f99359f1bab7201b3850e2
Andrei Cristian Petcu <andrei@ceata.org>
2014-08-19 22:34:03 +0300

    get meetings

--- app/helpers/justro_parser_helper.rb
+++ app/helpers/justro_parser_helper.rb
@@ -4,8 +4,9 @@ module JustroParserHelper
   def self.get_justro_meeting
     client = Savon.client(wsdl: 'http://portalquery.just.ro/query.asmx?WSDL')
     response = client.call(:cautare_sedinte, message: {institutie: 'CurteadeApelBUCURESTI', dataSedinta: '2013-03-05'})
-    puts response.body
+    meetings =  response.body[:cautare_sedinte_response][:cautare_sedinte_result][:sedinta]
+    # meetings =  response.body[:cautare_sedinte_response][:cautare_sedinte_result]
     # puts response.body[:cautare_sedinte_response][:cautare_sedinte_result][:sedinta][0][:departament] 
-    return response.body
+    return meetings
   end
 end
