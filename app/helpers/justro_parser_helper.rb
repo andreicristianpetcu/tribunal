f5c8ae194223e882332aa80779462aca1437616b
Andrei Cristian Petcu <andrei@ceata.org>
2014-08-21 01:10:48 +0300

    group request params in separate hash

--- app/helpers/justro_parser_helper.rb
+++ app/helpers/justro_parser_helper.rb
@@ -1,6 +1,22 @@
 require 'savon'
 
 module JustroParserHelper
+
+  def self.get_parser_request_params
+    request_params_list = []
+    court_name = 'CurteadeApelBUCURESTI'
+    startDate = Date.new(2000, 1, 1)
+    endDate = Date.today
+    (startDate..endDate).each do |date|
+      request_param = {}
+      request_param[:court_name] = court_name
+      request_param[:date] = date
+      request_params_list << request_param
+      # puts "date #{date}"
+    end
+    request_params_list
+  end
+
   def self.get_justro_meetings
     client = Savon.client(wsdl: 'http://portalquery.just.ro/query.asmx?WSDL')
     response = client.call(:cautare_sedinte, message: {institutie: 'CurteadeApelBUCURESTI', dataSedinta: '2013-03-05'})
@@ -13,6 +29,8 @@ module JustroParserHelper
     meetings.each do |meeting|
       puts meeting[:departament]
       requestDate = DateTime.now()
+      # requestParams = {}
+      # requestParZWams
       JustRoRequest.create(
         meeting: meeting, 
         requestDate: requestDate)
