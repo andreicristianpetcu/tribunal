fce8f40ae394177ba7f30c7df55843c97559d0d4
Andrei Cristian Petcu <andrei@ceata.org>
2014-09-08 21:46:27 +0300

    generate all request for files

--- app/models/justro_file_request_param.rb
+++ app/models/justro_file_request_param.rb
@@ -15,18 +15,24 @@ class JustroFileRequestParam
   end
 
   def self.generate_all
-    start_date = Date.new(2010, 1, 1)
+    start_date = Date.new(2013, 1, 1)
     end_date = Date.today - 1
+    # end_date = Date.today - 1
+
+    request = JustroFileRequestParam.new
 
     CourtDataProviderHelper.get_courts_names.each do |court_name|
-      # court_name = 'CurteadeApelBUCURESTI'
-
-      request = JustroFileRequestParam.new
-      request.court_name = court_name
-      request.start_date = start_date
-      request.end_date = end_date
+      (start_date..end_date).each do |date|
+        if(!date.saturday? && !date.sunday?) then
+          request = JustroFileRequestParam.new
+          request.court_name = court_name
+          request.start_date = date
+          request.end_date = date
+          # puts "Saving file request #{request.inspect}"
+          request.save
+        end
+      end
       puts "Saving file request #{request.inspect}"
-      request.save
     end
   end
 
