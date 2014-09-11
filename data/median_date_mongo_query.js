90124edb94796f535e9205b4ac32775c4522813e
Andrei Cristian Petcu <andrei@ceata.org>
2014-09-11 22:34:57 +0300

    small changes to query

--- data/median_date_mongo_query.js
+++ data/median_date_mongo_query.js
@@ -1,1 +1,1 @@
-db.trial_files.aggregate({$match: {court_id: ObjectId("53fba7656c6f6338e3010000")}}, {$limit: 200},{$group: { _id: "$number"}});
+db.trial_files.aggregate([{$limit: 200}, {$match: {court_id: ObjectId("53fba7656c6f6338e3010000")}}, {$group: { _id: "$number", }}, {$project : {date: 1, court_id: 1, _id: 1, number: 1}}]);
