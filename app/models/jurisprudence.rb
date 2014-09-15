299edeb7d9cd3fcf127022d2a5165146eb02a7a5
Andrei Cristian Petcu <andrei@ceata.org>
2014-09-15 20:12:01 +0300

    add jurisprudence

---
+++ app/models/jurisprudence.rb
@@ -0,0 +1,13 @@
+class Jurisprudence
+  include Mongoid::Document
+  field :titlu, type: String
+  field :tip, type: String
+  field :domeniu, type: String
+  field :decizie_sau_sentinta, type: Mongoid::Boolean
+  field :anul, type: Integer
+  field :sectia, type: String
+  field :scurta_descriere, type: String
+  field :descriere_completa, type: String
+
+  belongs_to :court
+end
---
+++ test/models/jurisprudence_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class JurisprudenceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
