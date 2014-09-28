cbe6e662cbdf47a7d0719e6d40e193be521b4e40
Andrei Cristian Petcu <andrei@ceata.org>
2014-09-28 13:27:12 +0200

    pretty circle markers

--- public/js/maps_with_leaflet.js
+++ public/js/maps_with_leaflet.js
@@ -1,13 +1,23 @@
 var show_court_performances_onmap = function(){
   court_performances.forEach(function(court) {
+    var indicator = court.indice;
     var lat = court.latitudine;
     var lon = court.longitudine;
-    var circle = L.circle([lat, lon], 5000, {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 5
-    }).addTo(map);
-    circle.bindPopup(court.localitate + "</br> Indice " + court.indice);
+
+    var colour = "#D9534F";
+    if(indicator > 0 && indicator < 1 ) {
+      colour = "#F8F774";
+    } else if (indicator > 1) {
+      colour = "#5CB85C";
+    }
+    var circle = L.circleMarker([lat, lon], {
+      color: "black",
+      fillColor: colour,
+      fillOpacity: 0.9, 
+      riseOnHover: true
+    });
+    circle.addTo(map);
+    circle.bindPopup(court.localitate + "</br> Indice " + indicator);
   });
 };
 show_court_performances_onmap();
