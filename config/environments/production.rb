b72f0ab67176610b04bc36c6454c0d8a32e24473
Andrei Cristian Petcu <andrei@ceata.org>
2014-12-05 22:27:34 +0200

    added evil ga tracking code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  GA.tracker = "UA-xxxxxx-x"
+  GA.tracker = "UA-57327806-1"
 end
